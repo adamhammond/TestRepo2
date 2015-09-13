66e5d485e204641de8cd397c16ede96670f7f115
adamhammond <ahammond@harding.edu>
2015-09-13 17:59:40 -0500

    Added VS Project

---
+++ WinForms/ModelessDialog.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace WinForms
+{
+    partial class ModelessDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(54, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(143, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.button1.Location = new System.Drawing.Point(88, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "OK";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ModelessDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(250, 113);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "ModelessDialog";
+            this.Text = "Modeless Dialog";
+            this.Activated += new System.EventHandler(this.ModelessDialog_Activated);
+            this.Deactivate += new System.EventHandler(this.ModelessDialog_Deactivate);
+            this.Load += new System.EventHandler(this.ModelessDialog_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ WinForms/ModelessDialog.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinForms
+{
+    public partial class ModelessDialog : Form
+    {
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        // Method Signature
+        public delegate void ChangedWordEvent(string word);
+        private Form1 _view;
+
+        // Event that is triggered when the word changes
+        public event ChangedWordEvent ChangedWord;
+
+        public void OnChangedWord(string word)
+        {
+            if (ChangedWord != null)
+            {
+                ChangedWord(word);
+            }
+        }
+        public ModelessDialog()
+        {
+            InitializeComponent();
+            textBox1.Text = "Test";
+            TopMost = true;
+        }
+
+        public ModelessDialog(Form1 form1)
+        {
+            InitializeComponent();
+            _view = form1;
+            _view.button1.Enabled = false;
+            textBox1.Text = form1.label1.Text;
+            TopMost = true;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            OnChangedWord(textBox1.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+            _view.button1.Enabled = true;
+        }
+
+        private void ModelessDialog_Load(object sender, EventArgs e)
+        {
+            SetForegroundWindow(this.Handle);
+        }
+
+        private void ModelessDialog_Deactivate(object sender, EventArgs e)
+        {
+            TopMost = true;
+            SetForegroundWindow(this.Handle);
+            Opacity = .75;
+        }
+
+        private void ModelessDialog_Activated(object sender, EventArgs e)
+        {
+            Opacity = 1;
+        }
+    }
+}
